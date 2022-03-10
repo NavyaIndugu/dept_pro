84072a7112ab562c2e4b843bbcb4629105634e29
NavyaIndugu <nindugu@osius.com>
2022-03-10 11:52:06 +0530

    my sec commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   resources :colleges
   get 'depts/aboutus'
   get 'depts/contact'
+  get 'depts/deptinfo'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #devise_for :users
-  root to: "depts#index"
+  root to: "depts#deptinfo"
   resources :depts do
    
    member do
